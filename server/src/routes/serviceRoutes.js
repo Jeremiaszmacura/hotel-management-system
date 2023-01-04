2bb0fd19fa29f897d8f2774b6bb434d29451c7fa
Jeremiaszmacura <jeremiaszmacura99@gmail.com>
2023-01-04 13:04:15 +0100

    services routes and controllers

--- server/src/routes/serviceRoutes.js
+++ server/src/routes/serviceRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router()
 
 
 router.get('/', serviceController.getAll)
+router.get('/:id', serviceController.getOne)
+router.post('/', isLoggedIn, serviceController.createService)
+router.put('/:id', isLoggedIn, serviceController.updateService)
+router.delete('/:id', isLoggedIn, serviceController.removeService)
 
 
 module.exports = router
